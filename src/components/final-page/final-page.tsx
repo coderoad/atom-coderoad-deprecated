6008217a3826d0074b9c4a46281cb3b4ad1cf093
ShMcK <shawn.j.mckay@gmail.com>
2016-03-06 18:18:36 -0800

    final-page component

--- src/components/final-page/final-page.tsx
+++ src/components/final-page/final-page.tsx
@@ -1,7 +1,20 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import {Paper, Divider} from 'material-ui';
+import {Paper, Divider, Card, CardTitle, CardText, CardActions, FlatButton} from 'material-ui';
 
 export const FinalPage = () => (
-  <div>Final Page</div>
+  <Paper>
+    <Card>
+      <CardTitle title='Congratulations!'
+      subtitle='Tutorial Complete!' />
+      <CardText>
+        What's next?
+
+        <CardActions>
+          <a href='https://coderoad.github.io/#tutorials'><FlatButton label='See More Tutorials' disabled={true}/></a>
+          <a href='https://coderoad.github.io/build'><FlatButton label='Learn how to Create a Tutorial' /></a>
+        </CardActions>
+      </CardText>
+    </Card>
+  </Paper>
 );
