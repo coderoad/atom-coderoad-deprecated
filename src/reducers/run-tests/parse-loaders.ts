3e3d29aa1d0b464a6300de4a0bea53d098d08183
ShMcK <shawn.j.mckay@gmail.com>
2016-04-01 14:15:29 -0700

    change to inline parse loaders

--- src/reducers/run-tests/parse-loaders.ts
+++ src/reducers/run-tests/parse-loaders.ts
@@ -2,9 +2,9 @@ import * as path from 'path';
 import * as fs from 'fs';
 
 const supportedFileTypes = ['js', 'jsx', 'ts', 'py'];
-const js = /^\/\/\s?load\(['"`](.+)['"`]\)$/m;
+const js = /^\/\/\s?load\(['"`](.+)['"`]\)$/m; // // load('file'),
 const loaderMatch = {
-  js,  // // load('file'),
+  js,
   ts: js,
   jsx: js,
   py: /^#\s?load\(['"`](.+)['"`]\)$/m      // # load('file')
@@ -17,10 +17,11 @@ export default function parseLoaders(data: string, fileType: string) {
     return '';
   }
 
-  let imports = '';
   // loop over lines and add editor files
   let i = -1;
   let lines = data.split('\n');
+
+  let filesLoaded = [];
 
   while (i < lines.length - 1) {
     i += 1;
@@ -29,17 +30,15 @@ export default function parseLoaders(data: string, fileType: string) {
     if (loader) {
       // loader found
       let fileToLoad: string = loader[1];
+
+      if (filesLoaded.indexOf(fileToLoad) > -1) {
+        console.log(`"${fileToLoad}" already loaded.`);
+        continue;
+      }
+
       let pathToFile: string = path.normalize(path.join(window.coderoad.dir, fileToLoad));
-      let file: string = fs.readFileSync(pathToFile, 'utf8');
-      imports += file;
+      lines[i] = fs.readFileSync(pathToFile, 'utf8');
     }
   }
-
-  let output: string = null;
-  if (imports.length > 0) {
-    output = imports + '\n'.concat(data);
-  } else {
-    output = data;
-  }
-  return output;
+  return lines.join('\n');
 }
