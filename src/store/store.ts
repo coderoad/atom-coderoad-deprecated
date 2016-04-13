c2e444a321022a8855a9e0c6caa1f40ab10cd254
ShMcK <shawn.j.mckay@gmail.com>
2016-04-13 15:57:37 +0800

    cleanup store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,11 +1,4 @@
-// import finalCreateStore from './middleware';
 import {createStore} from 'redux';
-// import {batchedSubscribe} from 'redux-batched-subscribe';
 import reducer from '../reducers/reducer';
 
-// const createStoreWithBatching = batchedSubscribe(
-//   fn => fn()
-// )(finalCreateStore);
-
 export let store: Redux.Store = createStore(reducer, {});
-// export const store: Redux.Store = createStore(reducer, initialState);
