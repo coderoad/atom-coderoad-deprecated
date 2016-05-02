fdadec87eb0ab94b5212c469239fb07bde160150
ShMcK <shawn.j.mckay@gmail.com>
2016-05-02 10:25:05 -0700

    remove redux logger

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,20 +1,8 @@
-import {createStore, applyMiddleware} from 'redux';
+import {createStore} from 'redux';
 import reducer from '../reducers';
-import * as createLogger from 'redux-logger';
 
-const dev = false;
-
-let store: Redux.Store = null;
-if (dev) {
-  const logger = createLogger();
-  store = createStore(
-    reducer,
-    applyMiddleware(logger)
-  );
-} else {
-  const initialState = {};
-  store = createStore(
+const initialState = {};
+const store = createStore(
     reducer, initialState
   );
-}
 export default store;
