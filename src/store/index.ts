9d02a1ab588151ee59ded166226b913f54e66ef5
ShMcK <shawn.j.mckay@gmail.com>
2016-05-12 20:05:08 -0700

    use redux-action-thunk

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,13 @@
 import { applyMiddleware, createStore } from 'redux';
+import {ratMiddleware} from 'redux-action-thunk';
 import reducer from '../reducers';
-import thunk from 'redux-thunk';
 import * as createLogger from 'redux-logger';
+import thunk from 'redux-thunk';
 
-const middlewares = [thunk];
+// const middlewares = [ramMiddleware];
+const middlewares = [ratMiddleware, thunk];
 
-const devMode = true;
+const devMode = false;
 if (devMode) {
   const logger = createLogger();
   middlewares.push(logger);
