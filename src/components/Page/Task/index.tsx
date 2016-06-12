20fbe11f9346f5e04238061107c217758449ab70
ShMcK <shawn.j.mckay@gmail.com>
2016-06-11 21:59:24 -0700

    refactor TaskCheckbox using selectors

--- src/components/Page/Task/index.tsx
+++ src/components/Page/Task/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import {connect} from 'react-redux';
 import {Markdown} from '../../index';
-import taskCheckbox from './taskCheckbox';
+import TaskCheckbox from './taskCheckbox';
 import {ListItem} from 'material-ui/List';
 import {lightGreen200, orange200} from 'material-ui/styles/colors';
 
@@ -27,7 +27,6 @@ const styles = {
 
 @connect((state, props) => ({
   testRun: state.testRun,
-  isCurrentTask: state.taskPosition === props.index,
   isCompletedTask: state.taskPosition > props.index,
 }))
 export default class Task extends React.Component<{
@@ -42,7 +41,7 @@ export default class Task extends React.Component<{
         key={index}
         style={Object.assign({}, styles.task, {backgroundColor})}
       >
-        {taskCheckbox(isCurrentTask, testRun)}
+        <TaskCheckbox index={index} />
         <span style={styles.index}>{index + 1}.</span>
         <div style={styles.description}>
           <Markdown >{task.description}</Markdown>
--- src/components/Page/Task/taskCheckbox.tsx
+++ src/components/Page/Task/taskCheckbox.tsx
@@ -1,19 +1,30 @@
 import * as React from 'react';
+import {connect} from 'react-redux';
 import {green500, orange500} from 'material-ui/styles/colors';
 import CheckBox from 'material-ui/svg-icons/toggle/check-box';
 import CheckBoxOutlineBlank from 'material-ui/svg-icons/toggle/check-box-outline-blank';
 import IndeterminateCheckBox from 'material-ui/svg-icons/toggle/indeterminate-check-box';
 
 const styles = {
-  position: 'absolute',
-  top: '15px'
+  checkbox: {
+    position: 'absolute',
+    top: '15px',
+  },
 };
 
-export default function taskCheckbox(isCurrentTask: boolean, testRun: boolean) {
-  if (!isCurrentTask || !testRun) { return null; }
-  return (
-    <span style={styles}>
-      <IndeterminateCheckBox color={orange500} />
-    </span>
-  );
-};
+@connect((state, props) => ({
+  testRun: state.testRun,
+  isCurrentTask: state.taskPosition === props.index,
+}))
+export default class TaskCheckbox extends React.Component<{
+  testRun?: boolean, isCurrentTask?: boolean, index: number
+}, {}> {
+  render() {
+    const {testRun, isCurrentTask} = this.props;
+    if (!isCurrentTask || !testRun) { return null; }
+    return <IndeterminateCheckBox
+      color={orange500}
+      style={styles.checkbox}
+    />;
+  }
+}
