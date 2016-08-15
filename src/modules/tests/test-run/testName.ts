2113897266548edbfc123a6b69ace3c4740e27a4
ShMcK <shawn.j.mckay@gmail.com>
2016-08-14 17:07:44 -0700

    set test names to end in two digits for file organization

--- src/modules/tests/test-run/testName.ts
+++ src/modules/tests/test-run/testName.ts
@@ -1,8 +1,12 @@
 import { join } from 'path';
 
+const twoDigitify = n => n > 9 ? '' + n : '0' + n;
+
 export default function getTestName({tutorial, pagePosition}): string {
   if (!tutorial || !tutorial.name || !tutorial.version || typeof pagePosition !== 'number') {
     console.log('Error creating temporary test name');
   }
-  return `${tutorial.name}__${tutorial.version}__${pagePosition}`;
+  return `${tutorial.name}__${tutorial.version}__${
+    twoDigitify(pagePosition + 1)
+  }`;
 }
