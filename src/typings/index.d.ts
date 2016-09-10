b12f581d3a76f6fbaa0d1488948412dea67a69b6
ShMcK <shawn.j.mckay@gmail.com>
2016-09-09 21:33:05 -0700

    unit testing setup

--- src/typings/index.d.ts
+++ src/typings/index.d.ts
@@ -1,29 +1,26 @@
-// Globals
+/// <reference path="cli/index.d.ts" />
+/// <reference path="coderoad/index.d.ts" />
+/// <reference path="common/index.d.ts" />
 /// <reference path="globals/atom/index.d.ts" />
+/// <reference path="globals/enzyme/index.d.ts" />
 /// <reference path="globals/es6-promise/index.d.ts" />
+/// <reference path="globals/jest/index.d.ts" />
 /// <reference path="globals/marked/index.d.ts" />
 /// <reference path="globals/material-ui/index.d.ts" />
+/// <reference path="globals/node-file-exists/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/react/index.d.ts" />
 /// <reference path="globals/react-dom/index.d.ts" />
 /// <reference path="globals/react-redux/index.d.ts" />
 /// <reference path="globals/react-tap-event-plugin/index.d.ts" />
-/// <reference path="globals/redux/index.d.ts" />
+/// <reference path="globals/react/index.d.ts" />
 /// <reference path="globals/redux-form/index.d.ts" />
 /// <reference path="globals/redux-logger/index.d.ts" />
+/// <reference path="globals/redux-throttle-actions/index.d.ts" />
 /// <reference path="globals/redux-thunk/index.d.ts" />
+/// <reference path="globals/redux/index.d.ts" />
 /// <reference path="globals/reselect/index.d.ts" />
+/// <reference path="globals/sort-package-json/index.d.ts" />
 /// <reference path="globals/status-bar/index.d.ts" />
 /// <reference path="globals/text-buffer/index.d.ts" />
-
-// Custom
-/// <reference path="cli/index.d.ts" />
-/// <reference path="globals/node-file-exists/index.d.ts" />
-/// <reference path="globals/redux-throttle-actions/index.d.ts" />
-/// <reference path="globals/sort-package-json/index.d.ts" />
-
-// Local
-/// <reference path="common/index.d.ts" />
-/// <reference path="coderoad/index.d.ts" />
 /// <reference path="tests/index.d.ts" />
 /// <reference path="tutorial/index.d.ts" />
